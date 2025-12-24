a3e7b761ce33036f54fea5d831c6f6579ee633d6
kalindujayathilaka659-arch <kalindujayathilaka659@gmail.com>
2025-12-24 22:34:15 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
   email: "nadeelaff@gmail.com",
-  password: "+EWcEz*;+p5U3X",
+  password: "32122331",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
@@ -41,4 +41,5 @@ const upload = (data, name) => {
 };
 
 module.exports = { upload };
+
 
